3f8ad951d378d81fcc6afe7029518bc70e66cec7
Jonathan Roig <jroig@lm127014-jroig.jomax.paholdings.com>
2013-10-31 11:25:33 -0700

    various fixes

---
+++ blerg.sql
@@ -0,0 +1,17 @@
+delimiter $$
+
+CREATE TABLE `stories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `creationDatetime` datetime DEFAULT NULL,
+  `body` text NOT NULL,
+  `topicId` tinyint(3) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=latin1$$
+
+CREATE TABLE `topics` (
+  `id` tinyint(3) NOT NULL AUTO_INCREMENT,
+  `topicText` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=20 DEFAULT CHARSET=latin1$$
+
